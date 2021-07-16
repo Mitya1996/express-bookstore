0689b90a10e2d29721f6bcbeaa5be4f73c2ce45a
mits <dmitrybaycharov@gmail.com>
2021-07-16 12:46:46 -0500

    all tests pass

--- data.sql
+++ data.sql
@@ -1,3 +1,18 @@
+\c books
+
+CREATE TABLE books (
+  isbn TEXT PRIMARY KEY,
+  amazon_url TEXT,
+  author TEXT,
+  language TEXT, 
+  pages INTEGER,
+  publisher TEXT,
+  title TEXT, 
+  year INTEGER
+);
+
+\c books_test
+
 CREATE TABLE books (
   isbn TEXT PRIMARY KEY,
   amazon_url TEXT,
